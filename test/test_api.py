fcee43b0e8db5ec302fefad1adad39d4c19a255b
friz25 <friz25@gmail.com>
2025-02-08 10:40:03 +0200

    3.3.2 тест

--- test/test_api.py
+++ test/test_api.py
@@ -217,6 +217,7 @@ def test_open_s6(browser):
    homepage.click_galaxy_s6()
    product_page = ProductPage(browser)
    product_page.check_title_is('Samsung galaxy s6')
+   """лол"""
 
 # region === OldVer (До POM структуры)
 
